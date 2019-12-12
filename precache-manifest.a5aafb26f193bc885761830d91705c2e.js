1f73567b419e6d401756627e913d1540cc4d7bc4
GreyCat20142017 <nrz3siaatg81@mail.ru>
2019-12-12 15:01:16 +0300

    Updates

---
+++ precache-manifest.a5aafb26f193bc885761830d91705c2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecf3c491a47fec46d287a8390c1b0018",
+    "url": "/rtasksmui/index.html"
+  },
+  {
+    "revision": "b804069549eeb3cb8109",
+    "url": "/rtasksmui/static/js/2.60ae2741.chunk.js"
+  },
+  {
+    "revision": "1af3d1841aa6c566551cbc6126be80ee",
+    "url": "/rtasksmui/static/js/2.60ae2741.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22f75128a4694a46f2e1",
+    "url": "/rtasksmui/static/js/main.fd0754fa.chunk.js"
+  },
+  {
+    "revision": "12c98dcacb60b1c9dd25",
+    "url": "/rtasksmui/static/js/runtime-main.d2c33a9d.js"
+  }
+]);
